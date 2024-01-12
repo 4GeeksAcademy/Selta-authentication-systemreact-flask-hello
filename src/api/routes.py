edc53eb74253fd431bd7c3848fbe0e702cedac70
Selta7 <132694122+Selta7@users.noreply.github.com>
2024-01-12 15:01:35 +0000

    Changed routes and also need to change comp and pages

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,25 +1,66 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
+from flask import Flask, request, redirect, jsonify, url_for, Blueprint
 from api.models import db, User
-from api.utils import generate_sitemap, APIException
+from api.utils import generate_sitemap, APIException, get_hash, hash_password, verify_password
 from flask_cors import CORS
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, unset_jwt_cookies
+
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
 
-@api.route("/token", methods=["POST"])
-def create_token():
-    email = request.json.get("email", None)
-    password = request.json.get("password", None)
-    if email != "test" or password != "test":
-        return jsonify({"msg": "Bad email or password"}), 401
+@api.route("/signup", methods=["POST"])
+def signup():
+    
+    email = request.json.get("email")
+    password = request.json.get("password")
+    
+    hashed_password = hash_password(password)
 
+    new_user = User (email=email, password=hashed_password)
+    db.session.add (new_user)
+    db.session.commit()
+    
+    return jsonify({"success":"User created successfully"}), 200
+
+
+@api.route("/login", methods=["POST"])
+def signup():
+    
+    email = request.json.get("email", None)
+    password = request.json.get("password", None) 
+    user = User.query.filter_by (email=email).first()  
+
+    if not user or not verify_password (user.password, password):
+        return jsonify({"error": "Invalid email or password"}), 401
+    
     access_token = create_access_token(identity=email)
-    return jsonify(access_token=access_token)
+    return jsonify (access_token=access_token)
+
+@api.route("/any-route", methods=["GET"])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify (logged_in_as= current_user), 200
+
+@api.route("/get-hash", methods=["POST"])
+def handle_get_hash():
+    to_hash = request.json.get ("string")
+    return get_hash (to_hash)
+
+#missing private
+
+@api.route("/logout", methods=["POST"])
+def logout ():
+    current_user = get_jwt_identity()
+
+    @jwt_required()
+    def exec_logout()
+        response = jsonify ({"message": "Logout successfull"})
+        unset_jwt_cookies(response)
+        return response, 200
+    return exec_logout()
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -8,35 +8,32 @@ export const Login = () => {
 	const { email, setEmail } = useState("");
 	const { password, setPassword } = useState("");
 
-	const handleLogin = () => {
-		const opt = {
+	const handleSubmit = () => {
+		e.preventDefault();
+
+		const opt = await fetch ('/login',{
 			method: 'POST',
 			headers: {
-				"Content-Type": "application/json"
+				'Content-Type' : 'application/json'
 			},
-			body: JSON.stringify({
-				"email": email,
-				"password": password
-			})
-		};
-
-		fetch ('https://animated-telegram-jx9wp5656553q695-3001.app.github.dev/', opt)
-		.then(resp => {
-			if (resp.status === 200) return resp.json();
-			else alert("There has been an error");
-		})
-		.then()
-		.catch(error => {
-			console.error("There was some kind of error", error);
+			body: JSON.stringify({email, password})	
 		});
-	};
+
+		if (Response.ok) {
+			const {}
+		}
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Login</h1>
-			<input type="text" placeholder="email" value= {email} onChange={(e) => setEmail(e.target.value)}   />
-			<input type="password" placeholder="password" value= {password} onChange={(e) => setPassword(e.target.value)}  />
-			<button onClick={handleLogin}>Login</button>
-		</div>
-	);
+		<form onSubmit={handleSubmit} className="form-container">
+      <div className="form-group">
+        <label className="label">Email:</label>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className="input" />
+      </div>
+      <div className="form-group">
+        <label className="label">Password:</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" />
+      </div>
+      <button type="submit" className="submit-button">Login</button>
+    </form>
+  );
 };
